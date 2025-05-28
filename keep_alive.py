6c8748886c504aebd18e5b269ff3aaa8cf70d004
Ars24444 <mr.ars.mr@mail.ru>
2025-05-29 03:21:08 +0400

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,18 +1,21 @@
 from flask import Flask
 from threading import Thread
 
-app = Flask('')
-
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "I'm alive"
 
-
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
+@app.route('/run')
+def run_manual():
+    try:
+        from main import send_signals
+        send_signals()
+        return "Signal sent"
+    except Exception as e:
+        return f"Error occurred: {e}"
 
 def keep_alive():
-    t = Thread(target=run)
+    t = Thread(target=lambda: app.run(host='0.0.0.0', port=8080))
     t.start()
