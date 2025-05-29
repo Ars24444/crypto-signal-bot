265befb351a2fb96078312ff59967f985d6eb491
Ars24444 <mr.ars.mr@mail.ru>
2025-05-29 22:30:32 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from run_signal_logic import send_signals
+import sys
+import os
 
 app = Flask(__name__)
 
@@ -11,14 +13,19 @@ def home():
 def run_manual():
     try:
         print("➡️ Entered /run route")
+        sys.stdout.flush()
+
         send_signals()
+
         print("✅ Finished send_signals()")
+        sys.stdout.flush()
+
         return "Started", 200
     except Exception as e:
         print(f"❌ Error in /run route: {e}")
+        sys.stdout.flush()
         return f"Error occurred: {e}", 500
 
-if __name__ == "__main__":
-    import os
+if name == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
