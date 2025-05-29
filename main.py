689f9243441b24b8f8c4791e6f9d79efdf2a14fd
Ars24444 <mr.ars.mr@mail.ru>
2025-05-29 21:35:03 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
 from flask import Flask
-import threading
-import os
-
-from run_signal_logic import send_signals  # ✅ bring signal logic from new file
+from run_signal_logic import send_signals
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return "I'm alive"
 
-@app.route('/run')
+@app.route("/run")
 def run_manual():
     try:
-        threading.Thread(target=send_signals).start()
+        send_signals()  
         return "Started", 200
     except Exception as e:
         return f"Error occurred: {e}", 500
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
