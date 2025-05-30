b9e6fd6c24a94aabde9f6665b83b792b8b28f084
Ars24444 <mr.ars.mr@mail.ru>
2025-05-31 02:21:18 +0400

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ from run_signal_logic import send_signals
 
 app = Flask(__name__)
 
+@app.route("/", methods=["GET"])
+def home():
+    return "🟢 Crypto Signal Bot is online. Use /run to start.", 200
+
 @app.route("/run", methods=["GET"])
 def run():
     threading.Thread(target=send_signals).start()
     return "✅ Signal execution started!", 200
 
-if __name__ == "__main__":
+if name == "__main__":
     app.run(host="0.0.0.0", port=10000)
