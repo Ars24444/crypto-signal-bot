cda3370532d2640a8fa9254c54d0f2704708bf51
Ars24444 <mr.ars.mr@mail.ru>
2025-05-31 01:06:03 +0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,5 +1,25 @@
 from ta.trend import SMAIndicator
 from ta.momentum import RSIIndicator
+import requests
+import pandas as pd
+
+def get_data(symbol, interval='1h', limit=100):
+    url = "https://api.binance.com/api/v3/klines"
+    params = {
+        "symbol": symbol,
+        "interval": interval,
+        "limit": limit
+    }
+    response = requests.get(url, params=params)
+    data = response.json()
+
+    df = pd.DataFrame(data, columns=[
+        "timestamp", "open", "high", "low", "close", "volume",
+        "close_time", "quote_asset_volume", "num_trades",
+        "taker_buy_base", "taker_buy_quote", "ignore"
+    ])
+    df[["open", "high", "low", "close", "volume"]] = df[["open", "high", "low", "close", "volume"]].astype(float)
+    return df
 
 def is_strong_signal(df, btc_change_pct=0):
     if df is None or len(df) < 30:
