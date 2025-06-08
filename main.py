5030252d6905ee629dde24b65164b37e50e3dc1d
Ars24444 <mr.ars.mr@mail.ru>
2025-06-09 01:58:26 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 from flask import Flask
 import threading
 from run_signal_logic import send_signals
+from check_signal_result_runner import check_recent_signal_results
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
 def home():
-    return "🟢 Crypto Signal Bot is online. Use /run to start.", 200
+    return "🟢 Crypto Signal Bot is online.", 200
 
 @app.route("/run", methods=["GET"])
 def run():
     threading.Thread(target=send_signals).start()
     return "✅ Signal execution started!", 200
+
+@app.route("/check_result", methods=["GET"])
+def check_result():
+    threading.Thread(target=check_recent_signal_results).start()
+    return "✅ Trade result check started!", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
