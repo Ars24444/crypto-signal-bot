973465672ff873aa7b4c858b90914c95d9b4f15b
Ars24444 <mr.ars.mr@mail.ru>
2025-06-12 15:55:05 +0400

    Update run_signal_logic.py

--- run_signal_logic.py
+++ run_signal_logic.py
@@ -17,7 +17,7 @@ def send_signals(force=False):
     print("🚀 Signal function started")
 
     btc_df = get_data("BTCUSDT")
-    btc_change_pct = (btc_df["close"].iloc[-1] - btc_df["close"].iloc[-4]) / btc_df["close"].iloc[-4] * 100
+    btc_change_pct = (btc_df["close"].iloc[-1] - btc_df["close"].iloc[-2]) / btc_df["close"].iloc[-2] * 100
     btc_rsi = RSIIndicator(btc_df["close"]).rsi().iloc[-1]
 
     print(f"📊 BTC change: {btc_change_pct:.2f}% | BTC RSI: {btc_rsi:.2f}")
