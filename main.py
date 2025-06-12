bd33acab200bb2848a460dcfe962183d86c65eb7
Ars24444 <mr.ars.mr@mail.ru>
2025-06-13 03:15:16 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import threading
 from run_signal_logic import send_signals
 send_signals(force=True)
 from check_signal_result_runner import check_recent_signal_results
+from signal_logger import send_winrate_to_telegram
+
+send_winrate_to_telegram(last_n=50)
 
 app = Flask(__name__)
 
@@ -19,6 +22,10 @@ def run():
 def check_result():
     threading.Thread(target=check_recent_signal_results).start()
     return "✅ Trade result check started!", 200
+@app.route("/run_winrate", methods=["GET"])
+def run_winrate():
+    send_winrate_to_telegram(last_n=50)
+    return "✅ Winrate report sent", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=10000)
