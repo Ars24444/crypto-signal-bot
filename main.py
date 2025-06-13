76617f6701bd3c8897341bcc16f4aa9570c7b410
Ars24444 <mr.ars.mr@mail.ru>
2025-06-13 22:29:12 +0400

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ import threading
 from telegram import Bot
 from generate_summary import generate_summary
 
-TELEGRAM_TOKEN = "7842956033:AAFCHreV97rJH11mhNQUhY3thpA_LpS5tLs"
+TELEGRAM_TOKEN = "7842956033:AAFCHreV97rJH11mhNQUhY3thpA_LpS5tLs" 
 CHAT_ID = 5398864436
 
 bot = Bot(token=TELEGRAM_TOKEN)
 app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def home():
+    return "🟢 Flask is working!", 200
 
 @app.route("/send-summary", methods=["GET"])
 def send_summary():
