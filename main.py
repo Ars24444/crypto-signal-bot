a3fabcd0e7e6ab4434d0912690efa931133fdf70
Ars24444 <mr.ars.mr@mail.ru>
2025-06-13 21:51:23 +0400

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,15 @@ from generate_summary import generate_summary
 
 TELEGRAM_TOKEN = "7842956033:AAFCHreV97rJH11mhNQUhY3thpA_LpS5tLs"
 CHAT_ID = 5398864436
-bot = Bot(token=TELEGRAM_TOKEN)
 
+bot = Bot(token=TELEGRAM_TOKEN)
 app = Flask(__name__)
 
 @app.route("/send-summary", methods=["GET"])
 def send_summary():
-    message = generate_summary()
-    bot.send_message(chat_id=CHAT_ID, text=message)
-    return "📤 Summary sent", 200
+    try:
+        message = generate_summary()
+        bot.send_message(chat_id=CHAT_ID, text=message)
+        return "📤 Summary sent", 200
+    except Exception as e:
+        return f"❌ Error: {e}", 500
